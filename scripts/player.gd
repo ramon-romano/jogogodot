1a39aae4c10f064befa29a36b706089bf2b6f1a9
Ramon Romano <133810946+ramon-romano@users.noreply.github.com>
2025-03-06 21:32:50 -0300

    issoAi

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var anima: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anima: AnimatedSprite2D = $AnimatedSprite2D	
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
@@ -16,13 +16,13 @@ func _physics_process(delta: float) -> void:
 			anima.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anima.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
