bcbd7a29aaaea8621f4231f0a4116ac42f084dc9
Ramon Romano <133810946+ramon-romano@users.noreply.github.com>
2025-04-03 21:33:19 -0300

    Add files via upload

---
+++ scripts/level_end.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+@export var next_level: String
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,30 @@
 extends CharacterBody2D
 
-@onready var anima: AnimatedSprite2D = $AnimatedSprite2D	
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 150.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
+
+@export var max_jump_count = 2
+
+var jump_count = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
-		if velocity.x != 0: 
-			anima.play("walk")
-		elif velocity.x == 0:
-			anima.play("idle")
+		jump_count = 0
+		if velocity.x != 0:
+			anim.play("walk")
+		else:
+			anim.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
 		velocity.y = JUMP_VELOCITY
-		anima.play("jump")
+		jump_count += 1 
+		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -27,10 +33,28 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+
 	if velocity.x > 0:
-		anima.flip_h = false 
+		anim.flip_h = false
 	elif velocity.x < 0:
-		anima.flip_h = true
+		anim.flip_h = true
+		
+
 		
 	move_and_slide()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("DeathZone"):
+		call_deferred("reload_scene")
+	elif area.is_in_group("LevelEnd"):
+		var next_level = area.next_level
+		if next_level:
+			call_deferred("load_scene", next_level)
+		else:
+			push_error("Proxima fase não definida em EndLevel")
+
+
+func reload_scene():
+	get_tree().reload_current_scene()
+func load_scene(scene_name: String):
+	get_tree().change_scene_to_file("res://scenes/" + scene_name + ".tscn")
